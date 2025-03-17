200fe9ac85353915b088a1522002a6e6e423cd46
Aleem Talha <aleemtalha098@gmail.com>
2025-03-17 16:09:06 +0500

    2nd commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || "localhost"
+const host = process.env.HOST || "0.0.0.0";
 
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
